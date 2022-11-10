c72501742402906b05d061ab13aa0e54d72eb0f7
mapavaladares <marcospaulofsa14@gmail.com>
2022-11-09 21:36:30 -0300

    mapa commit

---
+++ Variaveis/variaveis-const.js
@@ -0,0 +1,6 @@
+// tipo screen const que n varia e let q assume +1 de valor;
+const primeiroNome = "Mapa";
+console.log(primeiroNome);
+// let também é number,ou seja assume valores numericos ;
+let segundoNome = "Paulo";
+console.log(segundoNome);
